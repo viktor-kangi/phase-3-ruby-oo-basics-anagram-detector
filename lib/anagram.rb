945ae1e96f6ffcc5f6699ad593827b761ec467f0
victor <victor.buthia@student.moringaschool.com>
2023-02-13 09:20:08 +0300

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+    def match(text)
+        text.select do |t|
+            t.split(//).sort == word.split(//).sort
+        end
+    end
+end 
